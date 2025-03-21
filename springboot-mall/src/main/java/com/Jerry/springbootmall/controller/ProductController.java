ef9131cefeff4c3234ae5c8d32d2c02b7c729a64
JerryNezumi <kennu9972@gmail.com>
2025-03-21 22:33:16 +0800

    無

--- springboot-mall/src/main/java/com/Jerry/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/Jerry/springbootmall/controller/ProductController.java
@@ -84,7 +84,6 @@ public class ProductController {
         //檢查 product 是否存在
         Product product = productService.getProductById(productId);
         if (product == null) {
-            222
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         //修改商品的數據
