1dfca8399110cdc135b81fa8892540adc2e5bbf8
JerryNezumi <kennu9972@gmail.com>
2025-03-21 22:49:15 +0800

    456

--- springboot-mall/src/main/java/com/Jerry/springbootmall/controller/OrderController.java
+++ springboot-mall/src/main/java/com/Jerry/springbootmall/controller/OrderController.java
@@ -44,7 +44,7 @@ public class OrderController {
         orderQueryParam.setUserId(userId);
         orderQueryParam.setLimit(limit);
         orderQueryParam.setOffset(offset);
-        123
+        456
 
         List<Order> orders = orderService.getOrders(orderQueryParam);
         Integer totalOrder = orderService.getCountOrder(orderQueryParam);
